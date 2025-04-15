73f92ab2bd9ef74530490237fa2c423b8fd9dea0
OnlineAcademy254 <richardkasyoki2018@gmail.com>
2025-04-15 14:49:06 +0300

    update

--- js/index.js
+++ js/index.js
@@ -5,3 +5,37 @@ const hamburger = document.querySelector(".hamburger");
         hamburger.classList.toggle("active");
         navmenu.classList.toggle("active");
     })
+
+    function myFunction(){
+        var dot = document.getElementById("dot");
+        var more = document.getElementById("more");
+        var btnmore = document.getElementById("btnmore");
+    
+        if (dot.style.display === "none"){
+            dot.style.display = "inline";
+            btnmore.innerHTML = "Read more";
+            more.style.display = "none";
+        } 
+        else {
+            dot.style.display = "none";
+            btnmore.innerHTML = "Read less";
+            more.style.display = "inline";
+        }
+    }
+    
+    function myFunction1(){
+        var dot = document.getElementById("dot1");
+        var more = document.getElementById("more1");
+        var btnmore = document.getElementById("btnmore1");
+    
+        if (dot.style.display === "none"){
+            dot.style.display = "inline";
+            btnmore.innerHTML = "Read more";
+            more.style.display = "none";
+        } 
+        else {
+            dot.style.display = "none";
+            btnmore.innerHTML = "Read less";
+            more.style.display = "inline";
+        }
+    }
